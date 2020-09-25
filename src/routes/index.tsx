9ea54158cb3dea20f91260cfaa0627bce3006f85
João batista <joao.junior@certsys.com.br>
2020-09-25 20:52:26 -0300

    feat: path movie details

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
-import Repository from '../pages/Repository';
+import Movie from '../pages/Movie/index';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/Github-Explorer" exact component={Dashboard} />
-    <Route path="/repositories/:repository+" component={Repository} />
+    {/* <Route path="/" exact component={} /> */}
+    <Route path="/Movie" component={Movie} />
   </Switch>
 );
 
