7003eedbb83627486ac4a77fe56dbd99b4977b54
doctorget <samuelsoares177778@gmail.com>
2020-09-25 23:40:08 -0300

    new component profile, casee bootstrap fun

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Card, Button } from 'react-bootstrap';
+
+// import api from '../../services/api';
+
+// import { } from './styles';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Profile: React.FC = () => {
+    return (
+
+        <div className="container">
+            <div className="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
+
+        </div>
+
+
+    );
+};
+
+export default Profile;
