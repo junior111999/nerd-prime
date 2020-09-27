ef0e1a67058c5e8ee0ae84e28263fbb990bd6060
João batista <joao.junior@certsys.com.br>
2020-09-27 20:07:25 -0300

    fix: api

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-// import  from '../pages/';
-import Movie from '../pages/Movies';
-
 const Routes: React.FC = () => (
   <Switch>
-    {/* <Route path="/" exact component={} /> */}
-    <Route path="/Movie" component={Movie} />
+    <Route path="/" exact component={} />
+    <Route path="/" component={} />
   </Switch>
 );
 
