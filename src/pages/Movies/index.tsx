9e4ae49705d38363e5f4fc8ab23ca041810f3920
doctorget <samuelsoares177778@gmail.com>
2020-10-01 18:36:09 -0300

    new component navbar for all pages

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-//import NavBar from '../../components/NavBar'
+import NavBar from '../../components/NavBar'
 
 import CardMovie from '../../components/CardMovie';
 
+import { Background } from './styles';
 // import { Container, Row, Col } from 'react-bootstrap';
 
 // import api from '../../services/api';
@@ -13,9 +14,10 @@ import CardMovie from '../../components/CardMovie';
 const Movie: React.FC = () => {
   return (
       <>
-      <div className="container">
+      <Background>
+        <NavBar></NavBar>
         <CardMovie></CardMovie>
-      </div>
+      </Background>
       
       </>
   );
