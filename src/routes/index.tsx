efab7ee2276bea68f57ebc5b5e4d117f4bf2658c
Junior Souza <joaobat1999@gmail.com>
2021-10-22 10:51:33 -0300

    fix: avisos do eslint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-// import  from '../pages/';
 import Movies from '../pages/Movies';
 import Profile from '../pages/Profile';
-//realiza o roteamento entre filmes e seus detalhes
+
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Movies} />
