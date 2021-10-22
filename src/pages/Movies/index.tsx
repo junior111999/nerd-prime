7eb41c466148ba1a72118c12253f495966241169
Junior Souza <joaobat1999@gmail.com>
2021-10-22 11:20:29 -0300

    fix: ajuste na pagina de filmes

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,35 +1,30 @@
 import React, { useEffect, useState } from 'react';
 
 import NavBar from '../../components/NavBar';
-
 import CardMovie from '../../components/CardMovie';
-
 import { Background } from './styles';
-// import { Container, Row, Col } from 'react-bootstrap';
 
 import api from '../../services/api';
-// import { } from './styles';
 
 const Movies: React.FC = () => {
   const [movies, setMovies] = useState([]);
+
   useEffect(() => {
-    const loadMovies = async () => {
+    function loadMovies() {
       const list = await api.getMoviesList();
-      console.log(list);
-      // setMovies(list);
-    };
+      setMovies(list);
+    }
     loadMovies();
   });
+
   return (
-    <>
-      <Background>
-        <NavBar />
-        {movies.map((item) => (
-          <h1>${item}</h1>
-        ))}
-        <CardMovie />
-      </Background>
-    </>
+    <Background>
+      <NavBar />
+      {movies.map((item) => (
+        <h1>${item}</h1>
+      ))}
+      <CardMovie />
+    </Background>
   );
 };
 
